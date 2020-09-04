5ff4182fb20fa317d7bee8217481f9028e435cbb
Ramin Mammadzada <remki-design@Hotmail.com>
2020-09-04 20:54:13 +0300

    Add tests for my_each_with_index method.

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -25,23 +25,47 @@ describe Enumerable do
 
   end
   describe "#my_each_with_index" do
-    
+    it "should print the elements in the array with their indexes" do
+      expect{[1,2,5,9,7,9,10].my_each_with_index { |x, index| puts x.to_s + " " + index.to_s}}.to output("1 0\n2 1\n5 2\n9 3\n7 4\n9 5\n10 6\n").to_stdout
+    end
+
+    it "should print the elements in the range with their indexes" do
+      expect{(1..5).my_each_with_index { |x, index| puts x.to_s + " " + index.to_s}}.to output("1 0\n2 1\n3 2\n4 3\n5 4\n").to_stdout
+    end
+
+    it "should return enumerator" do
+      expect((1..5).my_each_with_index).to be_an_instance_of Enumerator
+    end
+
+    it "should print keys, values and indexes of elements in hash" do
+      expect{{ 'num1'=>21, 'num2'=>34}.my_each_with_index {|(key, value), index| puts key.to_s + ">" + value.to_s + ">" + index.to_s}}.to output("num1>21>0\nnum2>34>1\n" ).to_stdout
+    end
+
+    it "should print the elements in the array of strings with their indexes" do
+      expect{["hello","World","Ramin","Youcef"].my_each_with_index { |x, index| puts x.to_s + " " + index.to_s}}.to output("hello 0\nWorld 1\nRamin 2\nYoucef 3\n").to_stdout
+    end
   end
+
   describe "#my_select" do
     
   end
+
   describe "#my_all" do
     
   end
+
   describe "#my_any" do
     
   end
+
   describe "#my_none" do
     
   end
+
   describe "#my_count" do
     
   end
+
   describe "#my_map" do
     
   end
